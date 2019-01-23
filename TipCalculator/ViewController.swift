4bc50710ee5d78f1faa5e1d3ed90f68b98f7dfaf
Juan Dominguez <j5doming@ucsd.edu>
2019-01-23 00:30:14 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Juan Dominguez on 1/23/19.
+//  Copyright © 2019 Juan Dominguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
